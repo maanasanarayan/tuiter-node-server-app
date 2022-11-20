cbc62433fc38bf9f4a9499a26e3879af9c335682
maanasanarayan <mnsnryn@gmail.com>
2022-11-19 21:10:57 -0800

    tuiter node app

---
+++ controllers/hello-controller.js
@@ -0,0 +1,9 @@
+const HelloController = (app) => {
+  app.get("/hello", (req, res) => {
+    res.send("Hello World!");
+  });
+  app.get("/", (req, res) => {
+    res.send("Welcome to Full Stack Development!");
+  });
+};
+export default HelloController;
